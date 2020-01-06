f2acd58a7b663b97cc153599555705f80f21a144
Abdulla Bashiroglu <abdullabashir32@gmail.com>
2020-01-06 10:51:09 +0100

    problem temporary solved, but it is not fast and maintainable way for sure

--- src/components/useritem/UserItem.jsx
+++ src/components/useritem/UserItem.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import uuid from 'uuid';
 import Button from '../elements/formelements/button/Button';
 
 function UserItem({ employee, confirmation }) {
@@ -15,15 +16,21 @@ function UserItem({ employee, confirmation }) {
           <td key={employee[key]}>{employee[key]}</td>
         )
       )} */}
-      {Object.keys(employee).map(key =>
-        key === 'id' ? (
-          <th key={employee[key]} scope="row">
-            {employee.id}
-          </th>
-        ) : (
-          <td key={employee[key]}>{employee[key]}</td>
-        )
-      )}
+      {Object.keys(employee)
+        .concat([`confirm${uuid()}`])
+        .map(key =>
+          key === 'id' ? (
+            <th key={employee[key]} scope="row">
+              {employee.id}
+            </th>
+          ) : key.startsWith('confirm') ? (
+            <td key={employee[key]}>
+              <Button classes=" mx-3 btn-success">Confirm</Button>
+            </td>
+          ) : (
+            <td key={employee[key]}>{employee[key]}</td>
+          )
+        )}
     </tr>
   );
 }
