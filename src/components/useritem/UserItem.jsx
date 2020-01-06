6231c71d0805d586247203d4aefb4845cf61dadd
Abdulla Bashiroglu <abdullabashir32@gmail.com>
2020-01-06 11:35:44 +0100

    detected problem in useritem file, it still preserve its existence

--- src/components/useritem/UserItem.jsx
+++ src/components/useritem/UserItem.jsx
@@ -3,6 +3,9 @@ import uuid from 'uuid';
 import Button from '../elements/formelements/button/Button';
 
 function UserItem({ employee, confirmation }) {
+  const commingArray = confirmation
+    ? Object.keys(employee).concat([`confirm${uuid()}`])
+    : Object.keys(employee);
   return (
     <tr>
       {/* {Object.keys(employee).map(key =>
@@ -16,21 +19,20 @@ function UserItem({ employee, confirmation }) {
           <td key={employee[key]}>{employee[key]}</td>
         )
       )} */}
-      {Object.keys(employee)
-        .concat([`confirm${uuid()}`])
-        .map(key =>
-          key === 'id' ? (
-            <th key={employee[key]} scope="row">
-              {employee.id}
-            </th>
-          ) : key.startsWith('confirm') ? (
-            <td key={employee[key]}>
-              <Button classes=" mx-3 btn-success">Confirm</Button>
-            </td>
-          ) : (
-            <td key={employee[key]}>{employee[key]}</td>
-          )
-        )}
+
+      {commingArray.map(key =>
+        key === 'id' ? (
+          <th key={employee[key]} scope="row">
+            {employee.id}
+          </th>
+        ) : key.startsWith('confirm') ? (
+          <td key={employee[key]}>
+            <Button classes=" mx-3 btn-success">Confirm</Button>
+          </td>
+        ) : (
+          <td key={employee[key]}>{employee[key]}</td>
+        )
+      )}
     </tr>
   );
 }
