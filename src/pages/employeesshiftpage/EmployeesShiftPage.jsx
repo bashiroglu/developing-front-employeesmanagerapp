2d95a79362dc040b1edd30878af747e5d6b445db
Abdulla Bashiroglu <abdullabashir32@gmail.com>
2020-01-12 18:21:38 +0100

    addded function of fetching from our API

--- src/pages/employeesshiftpage/EmployeesShiftPage.jsx
+++ src/pages/employeesshiftpage/EmployeesShiftPage.jsx
@@ -5,6 +5,12 @@ import Table from '../../components/table/Table';
 import DownloadButtons from '../../components/downloadbuttons/DownloadButtons';
 
 export class EmployeesShiftPage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      employees: []
+    };
+  }
   static defaultProps = {
     TableColumns: ['id', 'fullname', 'username', 'shift type', 'shift'],
     FilterOptions: [
@@ -13,7 +19,8 @@ export class EmployeesShiftPage extends Component {
       '6:00-14:00',
       '14:00-22:00',
       '22:00-6:00'
-    ],
+    ]
+    /* ,
     employees: [
       {
         id: '1',
@@ -29,15 +36,25 @@ export class EmployeesShiftPage extends Component {
         shift: '22:00-6:00',
         username: 'hasanmurad'
       }
-    ]
+    ] */
   };
+  getEmployees() {}
+  componentDidMount() {
+    fetch('http://localhost:3003/api/v1/users')
+      .then(response => response.json())
+      .then(response => {
+        console.log(response.users);
+        this.setState({ employees: response.users });
+      });
+  }
   render() {
-    const { FilterOptions, TableColumns, employees } = this.props;
+    // const { employees } = this.state;
+    const { FilterOptions, TableColumns } = this.props;
     return (
       <div>
         <Filter FilterOptions={FilterOptions} />
         <DownloadButtons />
-        <Table TableColumns={TableColumns} employees={employees} />
+        <Table TableColumns={TableColumns} employees={this.state.employees} />
       </div>
     );
   }
