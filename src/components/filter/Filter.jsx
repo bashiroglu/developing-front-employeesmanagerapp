fb704caec3ce70aa4d0f995cce7f439b8bcce679
Abdulla Bashiroglu <abdullabashir32@gmail.com>
2020-02-08 12:11:11 +0100

    improve Filter component

--- src/components/filter/Filter.jsx
+++ src/components/filter/Filter.jsx
@@ -1,8 +1,23 @@
 import React from 'react';
+import axios from 'axios';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
 
 import SelectInput from '../elements/formelements/input/SelectInput';
 
-function Filter({ FilterOptions, name, value, onChange }) {
+function Filter({
+  FilterOptions,
+  name,
+  datepicker,
+  labelContent,
+  shiftDate,
+  setShiftDate,
+  shift,
+  setShift
+}) {
+  const handleSubmit = e => {
+    e.preventDefault();
+  };
   return (
     <div className="container my-3">
       <form className="form-inline">
@@ -12,27 +27,38 @@ function Filter({ FilterOptions, name, value, onChange }) {
         <SelectInput
           id="filterselect"
           name={name}
-          onChange={onChange}
+          onChange={e => setShift(e.target.value)}
           filterOptions={FilterOptions}
-          value={value}
+          value={shift}
         />
 
         <label className="my-1 mr-2" htmlFor="inlineFormCustomSelectPref">
-          employees
+          {labelContent}
         </label>
+        {datepicker ? (
+          <DatePicker
+            selected={shiftDate ? new Date(shiftDate) : null}
+            onChange={setShiftDate}
+            onChangeRaw={e => e.preventDefault()}
+          />
+        ) : null}
 
-        <div className="custom-control custom-checkbox my-1 mr-sm-2">
+        {/* <div className="custom-control custom-checkbox my-1 mr-sm-2">
           <input
             type="checkbox"
             className="custom-control-input"
             id="customControlInline"
           />
-          {/* <label className="custom-control-label" for="customControlInline">
+          <label className="custom-control-label" for="customControlInline">
             Remember my preference
-          </label> */}
-        </div>
+          </label>
+        </div> */}
 
-        <button type="submit" className="btn btn-primary my-1 ml-auto">
+        <button
+          type="submit"
+          onClick={handleSubmit}
+          className="btn btn-primary my-1 ml-auto"
+        >
           Submit
         </button>
       </form>
