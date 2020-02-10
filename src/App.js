486329b816094d32c015bbc7e1d2721113a817de
Abdulla Bashiroglu <abdullabashir32@gmail.com>
2020-02-10 18:58:14 +0100

    add funct. of checking if user is admin or not

--- src/App.js
+++ src/App.js
@@ -18,38 +18,53 @@ import { AuthContext } from './utils/context/authContext';
 
 function App() {
   const { userObject } = useContext(AuthContext);
-  console.log(userObject);
 
   let routes;
   if (userObject.token) {
-    routes = (
-      <React.Fragment>
-        <Switch>
-          <Route exact path="/employees-list">
-            <AllUsersPage />
-          </Route>
-          <Route exact path="/employees-shifts">
-            <EmployeesShiftPage />
-          </Route>
-          <Route exact path="/sign-up-user-by-manager">
-            <SignUpUserByManagerPage />
-          </Route>
-          <Route exact path="/employee-confirm">
-            <NewlyAddedEmployeesPage />
-          </Route>
-          <Route exact path="/managers-list-page">
-            <ManagerListPage />
-          </Route>
-          <Route exact path="/bookshift">
-            <BookShiftPage />
-          </Route>
-          <Route exact path="/settings">
-            <SettingsPage />
-          </Route>
-          <Redirect to="/bookshift" />
-        </Switch>
-      </React.Fragment>
-    );
+    if (userObject.role === 'admin') {
+      routes = (
+        <React.Fragment>
+          <Switch>
+            <Route exact path="/employees-list">
+              <AllUsersPage />
+            </Route>
+            <Route exact path="/employees-shifts">
+              <EmployeesShiftPage />
+            </Route>
+            <Route exact path="/sign-up-user-by-manager">
+              <SignUpUserByManagerPage />
+            </Route>
+            <Route exact path="/employee-confirm">
+              <NewlyAddedEmployeesPage />
+            </Route>
+            <Route exact path="/managers-list-page">
+              <ManagerListPage />
+            </Route>
+            <Route exact path="/bookshift">
+              <BookShiftPage />
+            </Route>
+            <Route exact path="/settings">
+              <SettingsPage />
+            </Route>
+            <Redirect to="/employees-list" />
+          </Switch>
+        </React.Fragment>
+      );
+    } else {
+      routes = (
+        <React.Fragment>
+          <Switch>
+            <Route exact path="/bookshift">
+              <BookShiftPage />
+            </Route>
+            <Route exact path="/settings">
+              <SettingsPage />
+            </Route>
+            <Redirect to="/bookshift" />
+          </Switch>
+        </React.Fragment>
+      );
+    }
   } else {
     routes = (
       <React.Fragment>
