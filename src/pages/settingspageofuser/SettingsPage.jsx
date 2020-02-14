c769b46c97c2a720167075dc69bf886f10abc9ef
Abdulla Bashiroglu <abdullabashir32@gmail.com>
2020-02-14 10:55:51 +0100

    add displaying gotten data in settings page

--- src/pages/settingspageofuser/SettingsPage.jsx
+++ src/pages/settingspageofuser/SettingsPage.jsx
@@ -1,9 +1,35 @@
-import React from 'react';
+import React, { useEffect, useState, useContext } from 'react';
+import axios from 'axios';
 import Input from '../../components/elements/formelements/input/Input';
 import Button from '../../components/elements/formelements/button/Button';
 import UpdatePassword from '../../components/updatepassword/UpdatePassword';
+import useInputState from '../../utils/hooks/useInputState';
+import { AuthContext } from '../../utils/context/authContext';
 
 function SettingsPage() {
+  // const [loading, setLoading] = useState(false);
+  const { userObject } = useContext(AuthContext);
+  const [fullname, setFullname] = useState('');
+  const [email, setEmail] = useState('');
+  const [username, setUsername] = useState('');
+  console.log(userObject.email);
+
+  useEffect(() => {
+    async function getData() {
+      const response = await axios.get(
+        `http://localhost:3003/api/v1/users/details/${userObject.email}`
+      );
+      console.log(response.data);
+
+      const { email, fullname, username } = response.data.user;
+      console.log(email, fullname, username);
+
+      setFullname(fullname);
+      setEmail(email);
+      setUsername(username);
+    }
+    getData();
+  }, []);
   return (
     <div className="container">
       <div className="row">
@@ -15,12 +41,31 @@ function SettingsPage() {
             type="email"
             name="email"
             id="email"
+            value={email}
+            onChange={e => setEmail(e.target.value)}
             element="input"
             additionaltextid="notifythatyouwillneed"
             additionaltext="Please provide valid email for security purposes"
           />
-          <Input label="Name" type="text" id="name" element="input" />
           <Input
+            label="Name"
+            type="text"
+            id="fullname"
+            name="fullname"
+            element="input"
+            value={fullname}
+            onChange={e => setFullname(e.target.value)}
+          />
+          <Input
+            label="username"
+            type="text"
+            id="username"
+            name="username"
+            element="input"
+            value={username}
+            onChange={e => setUsername(e.target.value)}
+          />
+          {/* <Input
             label="Provide your group name"
             type="email"
             id="groupname"
@@ -46,7 +91,7 @@ function SettingsPage() {
             element="input"
             additionaltextid="bodytext"
             additionaltext="We need this information to provide you with T-shirt"
-          />
+          /> */}
           <Button classes="mx-3 btn-success">Update</Button>
           {/* <Button classes=" mx-3 btn-success">Sign Up all users</Button> */}
         </form>
