c17a45d7dd1f1e79dd93057ccb5133a2f5d4b719
Abdulla Bashiroglu <abdullabashir32@gmail.com>
2020-02-16 19:47:25 +0100

    select input label move to selectinput itself

--- src/components/elements/formelements/input/SelectInput.jsx
+++ src/components/elements/formelements/input/SelectInput.jsx
@@ -1,20 +1,32 @@
 import React from 'react';
 
-function SelectInput({ name, value, filterOptions, onChange,disabled }) {
+function SelectInput({
+  name,
+  value,
+  filterOptions,
+  onChange,
+  disabled,
+  labelContent
+}) {
   return (
-    <select
-      disabled={disabled}
-      name={name}
-      value={value}
-      onChange={onChange}
-      className="custom-select my-1 mr-sm-2"
-    >
-      {filterOptions.map((option, idx) => (
-        <option key={option} value={option}>
-          {option}
-        </option>
-      ))}
-    </select>
+    <>
+      <label className="my-1 mr-2" htmlFor="inlineFormCustomSelectPref">
+        {labelContent}
+      </label>
+      <select
+        disabled={disabled}
+        name={name}
+        value={value}
+        onChange={onChange}
+        className="custom-select my-1 mr-sm-2"
+      >
+        {filterOptions.map((option, idx) => (
+          <option key={option} value={option}>
+            {option}
+          </option>
+        ))}
+      </select>
+    </>
   );
 }
 
