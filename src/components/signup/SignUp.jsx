042589e24401e776ac83c8acc68baf62f10b46fb
Abdulla Bashiroglu <abdullabashir32@gmail.com>
2020-02-21 01:50:27 +0100

    added error and succes handler to signup page

--- src/components/signup/SignUp.jsx
+++ src/components/signup/SignUp.jsx
@@ -1,14 +1,14 @@
-import React, { useState, useContext } from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
 
 import Input from './../elements/formelements/input/Input';
 import Button from './../elements/formelements/button/Button';
 import useInputState from '../../utils/hooks/useInputState';
-import { AuthContext } from '../../utils/context/authContext';
 
 function SignUp() {
-  const { setUserObject } = useContext(AuthContext);
+  const [success, setSuccess] = useState('');
+  const [error, setError] = useState('');
   const [emailInputValue, setEmail] = useInputState('');
   const [password, setPassword] = useInputState('');
   const [groupname, setGroupname] = useInputState('');
@@ -18,107 +18,112 @@ function SignUp() {
   const [loading, setLoading] = useState(false);
   async function handleSignUpSubmit(e) {
     e.preventDefault();
-    setLoading(true);
-    const response = await axios.post(
-      'http://localhost:3003/api/v1/users/signup',
-      {
-        groupname: groupname,
-        email: emailInputValue,
-        password,
-        fullname: fullnameInputValue,
-        username: usernameInputValue
+    try {
+      setLoading(true);
+      const response = await axios.post(
+        'http://localhost:3003/api/v1/users/signup',
+        {
+          groupname: groupname,
+          email: emailInputValue,
+          password,
+          fullname: fullnameInputValue,
+          username: usernameInputValue
+        }
+      );
+      if (response.data.operation === 'success') {
+        setSuccess(
+          'your account has been created, but you need to wait approval of your manager. Then you can enter to you profile'
+        );
+        setLoading(false);
       }
-    );
-    setLoading(false);
-    const { token, email, userId, username, fullname } = response.data;
-    localStorage.setItem(
-      'userObject',
-      JSON.stringify({
-        userId,
-        token,
-        email,
-        username,
-        fullname
-      })
-    );
-    setUserObject(response.data);
-
-    // console.log(response.data.token, response.data.userId, response.data.email);
+    } catch (error) {
+      setError(error.response.data.message);
+      setLoading(false);
+    }
   }
   return (
     <form onSubmit={handleSignUpSubmit}>
-      <h2>I don't have account</h2>
-      <Input
-        label="Email"
-        type="email"
-        id="email"
-        element="input"
-        additionaltextid="notifythatyouwillneed"
-        additionaltext="Please provide valid email, you will need to access your account"
-        name="email"
-        value={emailInputValue}
-        onChange={setEmail}
-      />
-      <Input
-        label="Fullname"
-        type="text"
-        id="text"
-        element="input"
-        additionaltextid="notifythatyouwillneed"
-        additionaltext="Please provide valid fullname, you will need to access your account"
-        name="fullname"
-        value={fullnameInputValue}
-        onChange={setFullname}
-      />
-      <Input
-        label="Username"
-        type="text"
-        id="text"
-        element="input"
-        additionaltextid="notifythatyouwillneed"
-        additionaltext="Please provide valid Username, you will need to access your account"
-        name="username"
-        value={usernameInputValue}
-        onChange={setUsername}
-      />
-      <Input
-        label="Password"
-        type="password"
-        id="password"
-        element="input"
-        name="password"
-        value={password}
-        onChange={setPassword}
-      />
-      <Input
-        label="Password Confirm"
-        type="password"
-        id="passwordConfirm"
-        element="input"
-        name="passwordConfirm"
-        value={passwordConfirm}
-        onChange={setPasswordConfirm}
-      />
-      <Input
-        label="Provide your group name"
-        type="text"
-        id="groupname"
-        element="input"
-        additionaltextid="groupnamefromagency"
-        additionaltext="You can get your groupname from your agency"
-        name="groupname"
-        value={groupname}
-        onChange={setGroupname}
-      />
-      <div className="row">
-        <Button classes=" mx-3 btn-success">Sign up</Button>
-        <Button classes=" mx-3 btn-primary">
-          {loading ? 'your account has been creating' : 'Sign up'}
-        </Button>
-        <Link className="nav-link" to="/sign-in">
-          I do have account
-        </Link>
-      </div>
+      {success ? (
+        <div className="text-success">{success || success}</div>
+      ) : (
+        <>
+          <h2>I don't have account</h2>
+          <Input
+            label="Email"
+            type="email"
+            id="email"
+            element="input"
+            additionaltextid="notifythatyouwillneed"
+            additionaltext="Please provide valid email, you will need to access your account"
+            name="email"
+            value={emailInputValue}
+            onChange={setEmail}
+          />
+          <Input
+            label="Fullname"
+            type="text"
+            id="text"
+            element="input"
+            additionaltextid="notifythatyouwillneed"
+            additionaltext="Please provide valid fullname, you will need to access your account"
+            name="fullname"
+            value={fullnameInputValue}
+            onChange={setFullname}
+          />
+          <Input
+            label="Username"
+            type="text"
+            id="text"
+            element="input"
+            additionaltextid="notifythatyouwillneed"
+            additionaltext="Please provide valid Username, you will need to access your account"
+            name="username"
+            value={usernameInputValue}
+            onChange={setUsername}
+          />
+          <Input
+            label="Password"
+            type="password"
+            id="password"
+            element="input"
+            name="password"
+            value={password}
+            onChange={setPassword}
+          />
+          <Input
+            label="Password Confirm"
+            type="password"
+            id="passwordConfirm"
+            element="input"
+            name="passwordConfirm"
+            value={passwordConfirm}
+            onChange={setPasswordConfirm}
+          />
+          <Input
+            label="Provide your group name"
+            type="text"
+            id="groupname"
+            element="input"
+            additionaltextid="groupnamefromagency"
+            additionaltext="You can get your groupname from your agency"
+            name="groupname"
+            value={groupname}
+            onChange={setGroupname}
+          />
+
+          <div className="row">
+            <>
+              <div className="text-danger">{error || error}</div>
+              <Button classes=" mx-3 btn-primary">
+                {loading ? 'your account is creating' : 'Sign up'}
+              </Button>
+              <Link className="nav-link" to="/sign-in">
+                I do have account
+              </Link>
+            </>
+          </div>
+        </>
+      )}
     </form>
   );
 }
